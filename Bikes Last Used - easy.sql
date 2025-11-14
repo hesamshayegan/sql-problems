38165fd68745d96a5c10ca670862aa932c3dad0f
Hesam <hesamshayegan88@gmail.com>
2025-11-14 14:14:56 -0800

    updated previous queries

--- Bikes Last Used - easy.sql
+++ Bikes Last Used - easy.sql
@@ -1,6 +1,19 @@
 -- stratascratch - easy
+
+-- faster query
 SELECT bike_number, 
        MAX(end_time) as last_used
 FROM dc_bikeshare_q1_2012
 GROUP BY bike_number
 ORDER BY last_used DESC
+
+-- window function
+SELECT bike_number, end_time FROM (
+    SELECT
+       bike_number,
+       end_time,
+       ROW_NUMBER() OVER (PARTITION BY bike_number ORDER BY end_time DESC) AS rn
+    FROM dc_bikeshare_q1_2012
+    ) AS sub
+WHERE rn = 1
+ORDER BY end_time DESC
