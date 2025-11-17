f9c6d24aeec113e831a94d96ed8dda06d96ea639
Hesam <hesamshayegan88@gmail.com>
2025-11-17 11:49:34 -0800

    updated previous queries

--- Employee and Manager Salaries - medium.sql
+++ Employee and Manager Salaries - medium.sql
@@ -12,3 +12,14 @@ FROM employee
 JOIN cte_managers
 ON employee.manager_id = cte_managers.id
 WHERE employee.salary > cte_managers.salary;
+
+
+-- subquery
+SELECT e.first_name,
+       e.salary
+FROM employee e
+WHERE e.salary > (
+    SELECT salary
+    FROM employee
+    WHERE id = e.manager_id
+    );
